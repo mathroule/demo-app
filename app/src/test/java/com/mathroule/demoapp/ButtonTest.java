2c97cd3fe745f4b8740f0fada7a3a404435e0eaa
Mathieu Rul <mathroule@gmail.com>
2019-09-23 00:06:21 +0200

    Add button test

---
+++ app/src/test/java/com/mathroule/demoapp/ButtonTest.java
@@ -0,0 +1,13 @@
+package com.mathroule.demoapp;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ButtonTest {
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(3, 2 + 2);
+    }
+}
